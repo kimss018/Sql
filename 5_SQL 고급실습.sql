296ae821d8905500733654a0acc0fa7cbc73ebc8
kimss018 <kimss018@naver.com>
2022-06-15 15:09:27 +0900

    고급실습

--- 5_SQL 고급실습.sql
+++ 5_SQL 고급실습.sql
@@ -1,4 +1,4 @@
-# 날짜 : 2022/06/14
+# 날짜 : 2022/06/15
 # 이름 : 김효경
 # 내용 : SQL 고급실습
 
@@ -7,15 +7,98 @@
 CREATE TABLE `Member`(
 `uid`   VARCHAR(10) PRIMARY KEY,
 `mane`  VARCHAR(10) NOT NULL,
-`hp`    CHAR(13)  UNIQUE,
-`pos`   VARCHAR(10) NOT NULL,
+`hp`    CHAR(13)  UNIQUE NOT NULL,
+`pos`   VARCHAR(10) NOT NULL DEFAULT '사원',
 `dep`   INT,
-`rdate` DATE
+`rdate` DATETIME NOT NULL 
 );
 
+DROP TABLE `Member`;
+
 CREATE TABLE `Department`(
-`depNo` INT NOT null,
-`name`  VARCHAR(10)
-`tel`
+`depNo` INT PRIMARY KEY,
+`name`  VARCHAR(10) NOT NULL,
+`tel`   CHAR(12) NOT NULL
 );
+
+CREATE TABLE `Sales`(
+`seq`     INT PRIMARY KEY AUTO_INCREMENT,
+`uid`     VARCHAR(10) NOT NULL,
+`year`    YEAR NOT NULL,
+`month`   TINYINT NOT NULL,
+`sale`    INT NOT NULL
+);
+
+# 실습하기 5-2
+
+INSERT INTO `Member` VALUES ('a101', '박혁거세', '010-1234-1001', '부장', 101, NOW());
+INSERT INTO `Member` VALUES ('a102', '김유신', '010-1234-1002', '차창', 101, NOW());
+INSERT INTO `Member` VALUES ('a103', '김춘추', '010-1234-1003', '사원', 101, NOW());
+INSERT INTO `Member` VALUES ('a104', '장보고', '010-1234-1004', '대리', 102, NOW());
+INSERT INTO `Member` VALUES ('a105', '강감찬', '010-1234-1005', '과장', 102, NOW());
+INSERT INTO `Member` VALUES ('a106', '이성계', '010-1234-1006', '차장', 103, NOW());
+INSERT INTO `Member` VALUES ('a107', '정철', '010-1234-1007', '차장', 103, NOW());
+INSERT INTO `Member` VALUES ('a108', '이순신', '010-1234-1008', '부장', 104, NOW());
+INSERT INTO `Member` VALUES ('a109', '허균', '010-1234-1009', '부장', 104, NOW());
+INSERT INTO `Member` VALUES ('a110', '정약용', '010-1234-1010', '사원', 105, NOW());
+INSERT INTO `Member` VALUES ('a111', '박지원', '010-1234-1011', '사원', 105, NOW());
+
+INSERT INTO `department` VALUES (101, '영업1부', '051-512-1001');
+INSERT INTO `department` VALUES (102, '영업2부', '051-512-1002');
+INSERT INTO `department` VALUES (103, '영업3부', '051-512-1003');
+INSERT INTO `department` VALUES (104, '영업4부', '051-512-1004');
+INSERT INTO `department` VALUES (105, '영업5부', '051-512-1005');
+INSERT INTO `department` VALUES (106, '영업지원부', '051-512-1006');
+INSERT INTO `department` VALUES (107, '인사부', '051-512-1007');
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2018, 1, 98100);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2018, 1, 136000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2018, 1, 80100);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2018, 1, 78000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2018, 1, 93000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2018, 2, 23500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2018, 2, 126000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2018, 2, 18500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2018, 2, 19000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2018, 2, 53000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a101', 2019, 1, 24000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2019, 1, 109000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2019, 1, 101000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2019, 1, 53500);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a107', 2019, 1, 24000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2019, 2, 160000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2019, 2, 101000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2019, 2, 43000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2019, 2, 24000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2019, 2, 109000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2020, 1, 201000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 1, 63000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 1, 74000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2020, 1, 122000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a107', 2020, 1, 111000);
+
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2020, 2, 120000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2020, 2, 39000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 2, 84000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 2, 180000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a108', 2020, 2, 76000);
+
+
+
+
+
+# 실습하기 5-3
+# 실습하기 5-4
+# 실습하기 5-5
+# 실습하기 5-6
+# 실습하기 5-7
+# 실습하기 5-8
+# 실습하기 5-9
+# 실습하기 5-10
+# 실습하기 5-11
+# 실습하기 5-12
 
