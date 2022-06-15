9a365156d0f4c93de9dbdf09f52d9289fd3ee35f
kimss018 <kimss018@naver.com>
2022-06-15 16:02:00 +0900

    고급실습

--- 5_SQL 고급실습.sql
+++ 5_SQL 고급실습.sql
@@ -6,14 +6,14 @@
 # 실습하기 5-1
 CREATE TABLE `Member`(
 `uid`   VARCHAR(10) PRIMARY KEY,
-`mane`  VARCHAR(10) NOT NULL,
+`name`  VARCHAR(10) NOT NULL,
 `hp`    CHAR(13)  UNIQUE NOT NULL,
 `pos`   VARCHAR(10) NOT NULL DEFAULT '사원',
 `dep`   INT,
 `rdate` DATETIME NOT NULL 
 );
 
-DROP TABLE `Member`;
+DROP TABLE `Sales`;
 
 CREATE TABLE `Department`(
 `depNo` INT PRIMARY KEY,
@@ -82,19 +82,84 @@ INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a106', 2020, 1, 12
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a107', 2020, 1, 111000);
 
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a102', 2020, 2, 120000);
-INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2020, 2, 39000);
+INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a103', 2020, 2, 93000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a104', 2020, 2, 84000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a105', 2020, 2, 180000);
 INSERT INTO `Sales` (`uid`, `year`, `month`, `sale`) VALUES ('a108', 2020, 2, 76000);
 
-
-
-
-
 # 실습하기 5-3
+
+SELECT * FROM `Member` WHERE `name`='김유신';
+SELECT * FROM `Member` WHERE `pos`='차장' AND dep=101;
+SELECT * FROM `Member` WHERE `pos`='차장' OR dep=101;
+SELECT * FROM `Member` WHERE `name`!='김춘추';
+SELECT * FROM `Member` WHERE `name`<>'김춘추';
+SELECT * FROM `Member` WHERE `pos`='차장' OR `pos`='대리';
+SELECT * FROM `Member` WHERE `pos` IN('사원', '대리');
+SELECT * FROM `Member` WHERE `dep` IN(101,102,103);
+SELECT * FROM `Member` WHERE `name` LIKE '김%';
+SELECT * FROM `Member` WHERE `name` LIKE '김__';
+SELECT * FROM `Member` WHERE `name` LIKE '_성_';
+SELECT * FROM `Member` WHERE `name` LIKE '정_';
+
+SELECT * FROM `Sales` WHERE `sale` > 50000;
+SELECT * FROM `Sales` WHERE `sale` >= 50000 AND `sale` < 100000 AND `month`=1;
+SELECT * FROM `Sales` WHERE `sale` BETWEEN 50000 AND 100000;
+SELECT * FROM `Sales` WHERE `sale` NOT between 50000 AND 100000;
+SELECT * FROM `Sales` WHERE `year` IN(2020);
+SELECT * FROM `Sales` WHERE `month` IN(1,2);
+
 # 실습하기 5-4
+
+SELECT * FROM `Sales` ORDER BY `sale`;
+SELECT * FROM `Sales` ORDER BY `sale` ASC;
+SELECT * FROM `Sales` ORDER BY `sale`DESC;
+
+SELECT * FROM `Member` ORDER BY `name`;
+SELECT * FROM `Member` ORDER BY `name` DESC;
+SELECT * FROM `Member` ORDER BY `name` ASC;
+
+SELECT * FROM `Sales` WHERE `sale` > 50000 ORDER BY `sale` DESC;
+SELECT * FROM `Sales` 
+    WHERE `sale` > 50000
+       ORDER BY `year`, `month`, `sale` DESC;
+
 # 실습하기 5-5
+
+SELECT * FROM Sales LIMIT 3; 
+SELECT * FROM Sales LIMIT 0, 3;
+SELECT * FROM Sales LIMIT 1, 3; 
+SELECT * FROM Sales LIMIT 4, 5;  
+SELECT * FROM sales ORDER BY `sale` desc LIMIT 3, 5;
+SELECT * FROM Sales WHERE `sale` < 50000 ORDER BY `sale` DESC LIMIT 3;
+SELECT * FROM sales
+    WHERE `sale` > 50000
+       ORDER BY `year` DESC, `month`, `sale` DESC
+          LIMIT 5; 
+
 # 실습하기 5-6
+
+SELECT SUM(sale) AS `합계` FROM `Sales`;
+SELECT AVG(sale) AS `평균` FROM `Sales`;
+SELECT MAX(sale) AS `최대값` FROM `Sales`;
+SELECT MIN(sale) AS `최소값` FROM `Sales`;
+SELECT COUNT(sale) AS `갯수` FROM `Sales`;
+SELECT COUNT(*) AS `갯수` FROM `Sales`;
+
+SELECT SUBSTRING(hp, 10, 4) AS '전화번호 끝자리' FROM `Member`;
+INSERT INTO `Member` VALUES ('b101', '을지문덕', '010-5555-1234', '사장', 107, NOW());
+## 확인문제 1
+SELECT SUM(`sale`) AS `2018년 1월 총매출` FROM `Sales` WHERE `year`=2018 AND `month`=1  
+
+## 확인문제 2
+SELECT SUM(sale) AS `총합`,
+       AVG(sale) AS `평균`,
+FROM `Salse` 
+WHERE `year`=2019 AND `month`=2 AND `sale` > 50000;
+
+## 확인문제 3
+SELECT MIN(sale), MAX(sale) FROM `Sales` WHERE `year`=2020;
+
 # 실습하기 5-7
 # 실습하기 5-8
 # 실습하기 5-9
