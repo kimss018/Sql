719e4eadd0d1c52e0f7de07c07be6f36b4301d96
kimss018 <kimss018@naver.com>
2022-06-17 12:50:32 +0900

    교재예제

--- 교재 3장 정렬과 연산.sql
+++ 교재 3장 정렬과 연산.sql
@@ -32,8 +32,6 @@ CREATE TABLE `sample311` (
 `b` INT
 );
 
-DROP TABLE `sample3`;
-
 INSERT INTO `sample311` VALUES ('1', 1);
 INSERT INTO `sample311` VALUES ('2', 2);
 INSERT INTO `sample311` VALUES ('10', 10);
@@ -150,19 +148,73 @@ select * FROM `sample341`;
 SELECT `amount`, ROUND(amount) FROM `sample341`;
 
 #p128
+
+SELECT `amount`, ROUND(amount,1) FROM `sample341`;
+
 #p129
 
+SELECT `amount`, ROUND(amount,-2) FROM `sample341`;
 
 #테이블 생성/데이터 저장
-CREATE TABLE `sample35` ();
-#p132
+CREATE TABLE `sample35` (
+`no`       INT PRIMARY KEY, 
+`price`    VARCHAR(10),
+`quantity` INT,
+`unit`     VARCHAR(10)
+);
+
+INSERT INTO `sample35` VALUES (1, '100', 10, '개');
+INSERT INTO `sample35` VALUES (2, '230', 24, '캔');
+INSERT INTO `sample35` VALUES (3, '1980', 1, '장');
+
+#p132 
+
+SELECT CONCAT(quantity, unit) FROM `sample35`;
+
 #p137
+
+SELECT CURRENT_TIMESTAMP;
+
 #p139
 
+SELECT CURRENT_DATE + INTERVAL 1 DAY;
+
 
 #테이블 생성/데이터 저장
-CREATE TABLE `sample37` ();
+CREATE TABLE `sample37` (
+`a` INT
+);
+
+INSERT INTO `sample37` VALUES (1);
+INSERT INTO `sample37` VALUES (2);
+INSERT INTO `sample37` VALUES ();
+
 #p142
+
+SELECT a, case when a IS NULL then 0 ELSE a END `a(null=0)` FROM `sample37`;
+
 #p144
+
+SELECT a AS `코드`, 
+case when a = 1 then '남자'
+     when a = 2 then '여자'
+     ELSE '미지정'
+END AS `성별` FROM `sample37`;
+
 #p145
+
+SELECT a AS `코드`, 
+case a when 1 then '남자'
+       when 2 then '여자'
+       ELSE '미지정'
+END AS `성별` FROM `sample37`;
+
 #p147
+
+SELECT a AS `코드`, 
+case a when 1 then '남자'
+       when 2 then '여자'
+       when `a`=NULL then '데이터없음'
+       ELSE '미지정'
+END AS `성별` FROM `sample37`;
+
